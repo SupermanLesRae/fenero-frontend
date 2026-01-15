38ee04d6ce125979cee4b4d4bd8b022e84bea829
Les-Rae Superman <superman.lesrae111@gmail.com>
2026-01-15 11:30:46 +0200

    feat: career build

--- app/api/graphql.js
+++ app/api/graphql.js
@@ -2,17 +2,14 @@ import fetch from "cross-fetch";
 
 export default async function handler(req, res) {
   try {
-    const response = await fetch(
-      process.env.NEXT_PUBLIC_WORDPRESS_GRAPHQL_URL + "graphql",
-      {
-        method: req.method,
-        headers: {
-          "Content-Type": "application/json",
-          // Add auth headers if needed
-        },
-        body: req.method === "POST" ? JSON.stringify(req.body) : undefined,
-      }
-    );
+    const response = await fetch("https://13.60.181.6/graphql", {
+      method: req.method,
+      headers: {
+        "Content-Type": "application/json",
+        // Add auth headers here if needed
+      },
+      body: req.method === "POST" ? JSON.stringify(req.body) : undefined,
+    });
 
     const data = await response.json();
     res.status(200).json(data);

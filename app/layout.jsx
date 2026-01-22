c4d91f5a186a48baf7b0167db6245b7116237f26
Les-Rae Superman <superman.lesrae111@gmail.com>
2026-01-22 06:33:18 +0200

    feat: light mode only added

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@ import { Nunito } from "next/font/google";
 import "./globals.css";
 import TopLoadingBar from "@/components/utilities/TopLoadingBar";
 import { Toaster } from "@/components/ui/sonner";
+import Head from "next/head";
 
 const nunito = Nunito({
   subsets: ["latin"],
@@ -15,6 +16,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" style={{ colorScheme: "light" }}>
+      <Head>
+        <meta name="color-scheme" content="light" />
+      </Head>
       <body className={`${nunito.variable} antialiased bg-[#E1E1E1]`}>
         <TopLoadingBar />
         {children}

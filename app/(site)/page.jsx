6e13a511d4d155760351b41fe984918fbc4ad39c
Les-Rae Superman <superman.lesrae111@gmail.com>
2026-01-27 14:59:31 +0200

    testing review

--- app/(site)/page.jsx
+++ app/(site)/page.jsx
@@ -1,12 +1,4 @@
-import dynamic from "next/dynamic";
-
 export const revalidate = 60;
-
-/* const HomeHero = dynamic(() => import("@/components/sections/HomeHero"));
-const   WhoAreYou = dynamic(() => import("@/components/sections/WhoAreYou"));
-const Testimonials = dynamic(
-  () => import("@/components/sections/Testimonials"),
-); */
 
 import HomeHero from "@/components/sections/HomeHero";
 import WhoAreYou from "@/components/sections/WhoAreYou";

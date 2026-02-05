9aeb273d92ce2b86220f85f078bc5a1bfe1d92b7
Les-Rae Superman <superman.lesrae111@gmail.com>
2026-02-05 12:13:15 +0200

    updates

--- components/utilities/TaxCalculatorForm.jsx
+++ components/utilities/TaxCalculatorForm.jsx
@@ -83,14 +83,15 @@ export function TaxCalculatorForm() {
     }; */
 
     const payload = {
-      daily_rate: Number(data.dailyRate).toFixed(2),
-      days_worked: Number(data.workDays),
+      daily_rate: +Number(data.dailyRate).toFixed(2), // converts to 450.00 as a number
+      days_worked: +Number(data.workDays),
       payment_frequency: data.payFrequency,
       marital_status: data.maritalStatus,
-      pension_contribution: Number(data.pensionContribution || 0).toFixed(2),
-      expenses: Number(data.businessExpenses || 0).toFixed(2),
-      /* current_salary: Number(data.currentSalary || 0).toFixed(2), */
+      pension_contribution: +Number(data.pensionContribution || 0).toFixed(2),
+      expenses: +Number(data.businessExpenses || 0).toFixed(2),
+      /*  current_salary: +Number(data.currentSalary || 0).toFixed(2), */
     };
+
     console.log(payload);
 
     try {

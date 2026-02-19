3905b85ab80e550ff425d93e0285a36938c0c2f8
Les-Rae Superman <superman.lesrae111@gmail.com>
2026-02-19 10:16:42 +0200

    updates

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteUrl: "https://fenero-frontend.vercel.app",
+  siteUrl: "https://fenero-frontend-smoky.vercel.app/",
   generateRobotsTxt: true,
   sitemapSize: 50000, // default
   generateIndexSitemap: false,
